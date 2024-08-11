ffa236ed289bd6a83c88c142acc3198a14c373e9
m.lewes@egyptianbanks.net <minaanwer@192.168.1.7>
2024-08-11 23:08:04 +0300

    update

---
+++ src/app/components/home-content/Appointment.ts
@@ -0,0 +1,17 @@
+export interface Appointment {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    phone: string;
+    datetime: string;
+    doctor: string;
+    payment: string;
+    doctorImage: string;
+    Status:String
+}
+
+
+export interface DropdownOption {
+    label: string;
+}
--- src/app/components/home-content/home-content.component.ts
+++ src/app/components/home-content/home-content.component.ts
@@ -3,6 +3,7 @@ import {format} from "date-fns";
 import {enGB} from "date-fns/locale";
 import {AuthService} from "../../services/auth/AuthService";
 import { CommonModule } from '@angular/common';
+import {Appointment, DropdownOption} from "./Appointment";
 
 @Component({
   selector: 'app-home-content',
@@ -15,8 +16,28 @@ export class HomeContentComponent {
 
 
   isDropdownOpen = false;
-  selectedOption: string = '';
-  formattedDate: string;
+   formattedDate: string;
+
+   selectedOption = 'Select an option';
+
+
+  appointments: Appointment[] = [
+    { id: 1, name: 'Ahmed Al-Mansoori', age: 25, gender: 'Male', phone: '050-1234567', datetime: '16th Sep 2023 11:00AM', doctor: 'Dr. Tarun', payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png' , Status:'Confirmed' },
+    { id: 2, name: 'Fatima Al-Habsi', age: 44, gender: 'Female', phone: '052-2345678', datetime: '18th Sep 2023 11:00AM', doctor: 'Dr. Tarun', payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png', Status:'Confirmed' },
+    { id: 3, name: 'Mohamed Al-Sheikh', age: 30, gender: 'Male', phone: '055-3456789', datetime: '20th Sep 2023 12:00AM', doctor: 'Dr. Tarun', payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png', Status:'Confirmed' },
+    { id: 4, name: 'Amira Ibrahim Sayed', age: 30, gender: 'Female', phone: '058-4567890', datetime: '19th Sep 2023 11:00AM', doctor: 'Dr. Tarun', payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png' , Status:'Confirmed'},
+    { id: 5, name: 'Aisha Al-Sheikh', age: 30, gender: 'Female', phone: '054-5678901', datetime: '24th Sep 2023 10:00AM', doctor: 'Dr. Tarun',payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png' , Status:'Confirmed'},
+    { id: 6, name: 'Mohamad El Rashed', age: 30, gender: 'Male', phone: '054-5678901', datetime: '28th Sep 2023 8:00AM', doctor: 'Dr. Sham', payment: 'Cash', doctorImage: '/assets/icon/menu/dr.png', Status:'Confirmed' }
+  ];
+
+  // Static list of dropdown options with type
+  dropdownOptions: DropdownOption[] = [
+    { label: 'Option 1' },
+    { label: 'Option 2' },
+    { label: 'Option 3' }
+  ];
+
+
 
   constructor(protected authService: AuthService) {
 
@@ -42,6 +63,5 @@ export class HomeContentComponent {
       this.isDropdownOpen = false;
     }
   }
-
 
 }

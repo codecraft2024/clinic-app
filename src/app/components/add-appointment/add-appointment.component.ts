442bc6ebdf39a3cd7bc5ffcfd9814ae832cb9595
m.lewes@egyptianbanks.net <minaanwer@192.168.1.7>
2024-08-14 21:02:07 +0300

    adjust close modal

--- src/app/components/add-appointment/add-appointment.component.ts
+++ src/app/components/add-appointment/add-appointment.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialogActions, MatDialogContent, MatDialogRef} from "@angular/material/dialog";
 import {MatFormField} from "@angular/material/form-field";
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatOption, MatSelect} from "@angular/material/select";
 import {MatDatepicker, MatDatepickerToggle} from "@angular/material/datepicker";
 import {Appointment} from "../home-content/Appointment";
@@ -13,8 +13,8 @@ import {enGB} from "date-fns/locale";
 import {format} from "date-fns";
 
 @Component({
-  selector: 'app-modal-content',
-  standalone: true,
+    selector: 'app-modal-content',
+    standalone: true,
     imports: [
         MatDialogActions,
         MatDialogContent,
@@ -29,33 +29,29 @@ import {format} from "date-fns";
         NgForOf,
         NgIf
     ],
-  templateUrl: './add-appointment.component.html',
-  styleUrl: './add-appointment.component.scss'
+    templateUrl: './add-appointment.component.html',
+    styleUrl: './add-appointment.component.scss'
 })
-export class AddAppointmentComponent implements  OnInit{
-    newAppointment:Appointment = new Appointment();
+export class AddAppointmentComponent implements OnInit {
+    newAppointment: Appointment = new Appointment();
     doctors: string[] = [];
+
     ngOnInit(): void {
         this.doctors = this.appointmentService.getDoctors();
     }
 
-    constructor(private appointmentService:AppointmentService , public dialogRef: MatDialogRef<AddAppointmentComponent>) {
-
+    constructor(private appointmentService: AppointmentService, public dialogRef: MatDialogRef<AddAppointmentComponent>) {
     }
-
 
 
     saveAppointment(form: NgForm): void {
         if (form.valid) {
             this.appointmentService.AddAppointment(this.newAppointment);
             this.dialogRef.close();
-        } else {
-            // Handle form errors here (optional)
-            console.log('Form is invalid');
         }
     }
 
-    closeModal(){
+    closeModal() {
         this.dialogRef.close();
     }
 
